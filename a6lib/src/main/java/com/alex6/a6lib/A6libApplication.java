1d3b48a58e7e2d4430060f0f215b36e794770241
alex6kim <kimerum333@gmail.com>
2023-07-01 23:28:52 +0900

    First Commit of a6lib

---
+++ a6lib/src/main/java/com/alex6/a6lib/A6libApplication.java
@@ -0,0 +1,13 @@
+package com.alex6.a6lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A6libApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A6libApplication.class, args);
+	}
+
+}
